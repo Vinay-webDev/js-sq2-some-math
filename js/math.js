3de89ed136d0f46a4234034ca5f1402e5ff4736b
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-30 23:53:35 +0530

    maximum value in array

--- js/math.js
+++ js/math.js
@@ -1,3 +1,4 @@
+// check for palindrome
 function isPalindrome(str) {
     let cleanedStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
     let reversedStr = cleanedStr.split('').reverse().join('');
@@ -5,3 +6,15 @@ function isPalindrome(str) {
 }
 
 console.log(isPalindrome("A man, a plan, a canal, Panama")); // Output: true
+// maximum value in array
+function findMax(arr) {
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+console.log(findMax([1, 3, 7, 2, 5])); // Output: 7
