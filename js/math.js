58b7944dc9538c614f8923422fbd30f8790473a2
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-30 23:52:25 +0530

    check for palindrome

---
+++ js/math.js
@@ -0,0 +1,7 @@
+function isPalindrome(str) {
+    let cleanedStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    let reversedStr = cleanedStr.split('').reverse().join('');
+    return cleanedStr === reversedStr;
+}
+
+console.log(isPalindrome("A man, a plan, a canal, Panama")); // Output: true
